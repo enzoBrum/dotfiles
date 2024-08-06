9a75dae6cfae366e68a228cb954f2c71064a036e
Enzo Brum <darosabrumenzo@gmail.com>
2024-08-05 22:55:35 -0300

    update toolbox image;

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,22 +1,10 @@
 if status is-interactive
-  #fish_vi_key_bindings
-  #function reverse_history_search
-  #history | fzf --no-sort | read -l command
-  #if test $command
-  #commandline -rb $command
-  #end
-  #end
-  #
-  #function fish_user_key_bindings
-  #bind -M default / reverse_history_search
-  #end
   fish_default_key_bindings
     if command -v flatpak
       alias google-chrome-stable "flatpak run com.google.Chrome"
     else
       alias google-chrome-stable "distrobox-host-exec flatpak run com.google.Chrome"
     end
-    alias update "flatpak update -y && oolbox list --containers | sed '1d' | tr -s ' '  | cut -f 2 -d ' ' | xargs -I {} toolbox run --container {} sudo dnf5 update -y && cd $HOME/repos/dotfiles/container-images/silverblue && sudo make rebuild && cd -"
     if command -v fastfetch
         fastfetch
     end
@@ -30,12 +18,4 @@ if status is-interactive
     fzf_configure_bindings --directory=\cf
     alias cat "bat --style=plain --theme=\"Dracula\""
     alias ls "eza --icons"
-
-    # function dbx-create
-    #    distrobox-create --name $argv[1] --additional-packages "git neovim bat dnf5 tldr eza helix fastfetch" \
-    #                     --volume /var/home/erb/.config/fish:$DBX_CONTAINER_HOME_PREFIX/$argv[1]/.config/fish \
-    #                     --volume /var/home/erb/.config/helix:$DBX_CONTAINER_HOME_PREFIX/$argv[1]/.config/helix \
-    #                     
-    #    distrobox-enter $argv[1]
-    # end
 end
