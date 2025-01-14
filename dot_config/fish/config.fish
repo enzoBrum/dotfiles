f5e151a15ba70361715b90b32c42f08961e2290f
Enzo Brum <darosabrumenzo@gmail.com>
2025-01-14 19:11:32 -0300

    idk

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,22 +1,9 @@
 if status is-interactive
   fish_default_key_bindings
-    if command -v flatpak &> /dev/null
-      alias google-chrome-stable "flatpak run com.google.Chrome"
-    else
-      alias google-chrome-stable "distrobox-host-exec flatpak run com.google.Chrome"
-    end
-    if command -v fastfetch &> /dev/null
-        fastfetch
-    end
-    if command -v direnv &> /dev/null
-      direnv hook fish | source
-    end
-    if command -v grc &> /dev/null
-      source /etc/grc.fish
-    end
-    if command -v zellij &> /dev/null
-      zellij setup --generate-completion fish | source
-    end
+    fastfetch
+    direnv hook fish | source
+    source /etc/grc.fish
+    zellij setup --generate-completion fish | source
 
     fzf_configure_bindings --directory=\cf
     alias cat "bat --style=plain --theme=\"Dracula\""
