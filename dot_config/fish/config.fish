3398985153352086a1af8c7e8e7d1dae2760a53a
Enzo Brum <darosabrumenzo@gmail.com>
2025-02-23 19:25:24 -0300

    idk

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,14 +1,18 @@
 if status is-interactive
     if command -v flatpak &> /dev/null
       alias google-chrome-stable "flatpak run com.google.Chrome"
+      alias firefox "flatpak run org.mozilla.firefox"
     else
       alias google-chrome-stable "distrobox-host-exec flatpak run com.google.Chrome"
+      alias firefox "distrobox-host-exec flatpak run org.mozilla.firefox"
     end 
     fish_default_key_bindings
     fastfetch
     direnv hook fish | source
     source /etc/grc.fish
-    zellij setup --generate-completion fish | source
+    if command -v zellij &> /dev/null
+      zellij setup --generate-completion fish | source
+    end
 
     fzf_configure_bindings --directory=\cf
     alias cat "bat --style=plain --theme=\"Dracula\""
@@ -17,4 +21,5 @@ if status is-interactive
     set -gx PATH $HOME/.local/bin $PATH
     set -gx DBX_CONTAINER_IMAGE localhost/my-toolbox
     set -fx DISTROBOX__HOME /var/home/erb/containers
+    set -gx EDITOR nvim
 end
